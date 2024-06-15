d76a71c2ce66df7c9a02a7ee3759767b426d6741
kolabse <mikefedd@gmail.com>
2024-06-15 14:51:01 +0500

    + 0x128, fix for 0x036, 0x0F6

--- Can2004Adapter.cpp
+++ Can2004Adapter.cpp
@@ -5,6 +5,8 @@ void Can2004Adapter::decode(uint32_t id, uint8_t dlc, uint8_t data[8])
     // 0x036
     if (id == 0x036) {
         carState->setEconomyMode(data[2] >> 7);
+        carState->setLamp(CarState::Lamp::DASHBOARD_LIGHTNING, bitRead(data[3], 2));
+        carState->setDashboardBrightness(data[3] & 0b00001111);
         switch (data[4] & 0b00000111)
         {
             case 0b00000001:
@@ -17,6 +19,7 @@ void Can2004Adapter::decode(uint32_t id, uint8_t dlc, uint8_t data[8])
                 carState->setIgnitionMode(CarState::IgnitionMode::START);
                 break;
         }
+
     } 
     // 0x0B6
     else if (id == 0x0B6) {
@@ -26,11 +29,11 @@ void Can2004Adapter::decode(uint32_t id, uint8_t dlc, uint8_t data[8])
         carState->setFuelConsumptionCounter(data[6]);
     }
     // 0x0E6
-    else if(id == 0x0E6) {
+    else if (id == 0x0E6) {
         carState->setBatteryVoltage((data[6] + 144) / 20);
     }
     // 0x0F6
-    else if(id == 0x0F6) {
+    else if (id == 0x0F6) {
         switch (data[0] & 0b00001000)
         {
             case 0b00001000:
@@ -43,9 +46,23 @@ void Can2004Adapter::decode(uint32_t id, uint8_t dlc, uint8_t data[8])
         carState->setCoolantTemp(data[1] - 39);
         carState->setOdometerValue((data[2] << 16) + (data[3] << 8) + (data[4]));
         carState->setOutdoorTemp(round(data[6] / 2.0 - 39.5));
-        carState->setLamp(CarState::Lamp::REVERSE, data[7] & 0b10000000);
-        carState->setLamp(CarState::Lamp::RIGHT_TURN, data[7] & 0b00000010);
-        carState->setLamp(CarState::Lamp::LEFT_TURN, data[7] & 0b00000001);
+        carState->setLamp(CarState::Lamp::REVERSE, bitRead(data[7], 0));
+        carState->setLamp(CarState::Lamp::RIGHT_TURN, bitRead(data[7], 6));
+        carState->setLamp(CarState::Lamp::LEFT_TURN, bitRead(data[7], 7));
+    }
+    // 0x128
+    else if (id == 0x128) {
+        carState->setLamp(CarState::Lamp::DRIVER_BELT, bitRead(data[0], 1));
+        carState->setLamp(CarState::Lamp::PARKING_BRAKE_LIGHT, bitRead(data[0], 2));
+        carState->setLamp(CarState::Lamp::DOORS, bitRead(data[1], 3));
+        carState->setLamp(CarState::Lamp::SIDE_LIGHT, bitRead(data[4], 0));
+        carState->setLamp(CarState::Lamp::BEAM_LOW, bitRead(data[4], 1));
+        carState->setLamp(CarState::Lamp::BEAM_HIGH, bitRead(data[4], 2));
+        carState->setLamp(CarState::Lamp::FOG_FRONT, bitRead(data[4], 3));
+        carState->setLamp(CarState::Lamp::FOG_REAR, bitRead(data[4], 4));
+        carState->setLamp(CarState::Lamp::RIGHT_INDCATOR, bitRead(data[4], 5));
+        carState->setLamp(CarState::Lamp::LEFT_INDCATOR, bitRead(data[4], 6));
+        carState->setLamp(CarState::Lamp::FUEL_LOW, bitRead(data[5], 0));
     }
 }
 
